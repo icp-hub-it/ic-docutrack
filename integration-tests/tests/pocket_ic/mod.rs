7fc973be7acdd0e32b2f081f89b3171605e0c738
Christian Visintin <christian.visintin1997@gmail.com>
2025-05-26 10:54:12 +0200

    test: Added pocket_test::test macro to automatically setup the test env (#26)

--- integration-tests/tests/pocket_ic/mod.rs
+++ integration-tests/tests/pocket_ic/mod.rs
@@ -1,9 +1,5 @@
 mod orchestrator;
 mod user_canister;
 
-use integration_tests::PocketIcTestEnv;
-
-#[tokio::test]
-async fn test_should_setup_test_env() {
-    PocketIcTestEnv::init().await.stop().await;
-}
+#[pocket_test::test]
+async fn test_should_setup_test_env(env: PocketIcTestEnv) {}
